9bff2e37e400c2b6b9185285cf0da7ae9ba2c0d6
vince4128 <vrlt.vincent@gmail.com>
2019-06-04 17:24:07 +0200

    Testing presence of CommentBox
    Using the shallow Enzyme method to check the presence of CommentBox component into App

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
+import { exportAllDeclaration } from '@babel/types';
 
 it('shows a comment box', ()=>{
-    const div = document.createElement('div');
+    const wrapped = shallow(<App/>);
 
-    ReactDOM.render(<App/>, div);
-
-    // Looks inside the div 
-    // and checks to see if the CommentBox is in there
-    expect(div.innerHTML).toContain('CommentBox');
-
-    //cleanup
-    ReactDOM.unmountComponentAtNode(div);
+    expect(wrapped.find(CommentBox).length).toEqual(1);
 });
