edf07fa66024f455f220e7cfa8aa63b2f02dbcdc
vince4128 <vrlt.vincent@gmail.com>
2019-06-04 22:34:12 +0200

    Converting CommentBox to a class based component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const CommentBox = () => {
-    return <div>CommentBox</div>
+class CommentBox extends Component {
+
+    state={ comment: ''};
+
+    handleChange = (event) => {
+        this.setState({ comment:event.target.value });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        // TODO - call action creator
+        // and save the comment
+
+        this.setState({ comment: '' });
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a Comment</h4>
+                <textarea value={this.state.comment} onChange={this.handleChange}/>
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        )
+    }        
 }
 
 export default CommentBox;
