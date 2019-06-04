c555040a17a1b73c13129a0e4474fe74987db546
vince4128 <vrlt.vincent@gmail.com>
2019-06-04 22:33:28 +0200

    Avoid repetition in test
    using beforeEach to refactor the code and declare the wrapper just once in test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
-import { exportAllDeclaration } from '@babel/types';
+import CommentList from '../CommentList';
+
+let wrapped;
+
+beforeEach(()=>{
+    wrapped = shallow(<App/>);
+});
 
 it('shows a comment box', ()=>{
-    const wrapped = shallow(<App/>);
-
     expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', ()=>{
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });
