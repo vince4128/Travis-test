89f6cedb54175bcbe5cef1749bcc1e11e151f00e
vince4128 <vrlt.vincent@gmail.com>
2019-06-05 17:55:51 +0200

    Test adding a comment
     - Refactor with beforeEach and afterEach
    
     - unmount the component
    
     - simulate a text input
    
     - force re-render
    
     - test the textarea values

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -7,9 +7,28 @@ import CommentBox from '../CommentBox';
 // but for learning reason we use the full DOM render (mount)
 // (render the component and all of its children + let us modify it afterwards)
 
-it('has a text area and a button', ()=>{
-    const wrapped = mount(<CommentBox/>);
+let wrapped;
 
+beforeEach(() => {
+    wrapped = mount(<CommentBox/>);
+});
+
+afterEach(()=>{
+    wrapped.unmount();
+});
+
+it('has a text area and a button', ()=>{
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+
+    //force the component to re-render in order to test if it had received text value
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
