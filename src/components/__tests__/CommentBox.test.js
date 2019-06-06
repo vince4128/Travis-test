433786f4d070dd06368ddb5963271cb3c75ee352
vince4128 <vrlt.vincent@gmail.com>
2019-06-06 13:36:56 +0200

    Test submit event + refactor
    The textarea should be empty after a submission.
    The text area test has been moved into a describe bock to add a specific beforeEach statement

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from '../CommentBox';
 
-// we should use the shallow render 
+// shallow render 
 // (render *just* the given component and none of its children)
-// but for learning reason we use the full DOM render (mount)
+// full DOM render (mount)
 // (render the component and all of its children + let us modify it afterwards)
 
 let wrapped;
@@ -22,13 +22,25 @@ it('has a text area and a button', ()=>{
     expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment' }
+describe('the text area', ()=>{
+
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        });
+    
+        //force the component to re-render in order to test if it had received text value
+        wrapped.update();
     });
 
-    //force the component to re-render in order to test if it had received text value
-    wrapped.update();
+    it('has a text area that users can type in', () => {            
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
+    
+    it('when form is submitted, text area gets emptied', () => {
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
 
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
+})
