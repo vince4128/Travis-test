d8cee0b7095891a2b65a2b97922238a6348ec367
vince4128 <vrlt.vincent@gmail.com>
2019-06-07 17:02:10 +0200

    Import cleaned
    Unused combineReducers method deleted from imports

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import reducers from './reducers';
 
 const store = createStore(reducers, {});
