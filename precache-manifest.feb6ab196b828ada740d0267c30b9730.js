0774229e1f3002c597a7dee088fa3442875cde21
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-12 12:17:24 +0000

    Deploy vince4128/travis-test to github.com/vince4128/travis-test.git:gh-pages

---
+++ precache-manifest.feb6ab196b828ada740d0267c30b9730.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc98f48a612f1bec66b4b461c030f963",
+    "url": "/travis-test/index.html"
+  },
+  {
+    "revision": "8f4119318071eec74c0a",
+    "url": "/travis-test/static/js/2.996f4e1c.chunk.js"
+  },
+  {
+    "revision": "546c3a2e81dd1d8438ad",
+    "url": "/travis-test/static/js/main.3c9859f2.chunk.js"
+  },
+  {
+    "revision": "2d77005be239ea72ccee",
+    "url": "/travis-test/static/js/runtime~main.2689178f.js"
+  }
+]);
